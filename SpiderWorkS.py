83466becbba56ffb56f6baadc0e92a300d353f52
Moonlor <Kenneth0875@gmail.com>
2018-04-09 20:58:06 +0800

    修复在CentOs下chromedriver的运行问题

--- SpiderWorkS.py
+++ SpiderWorkS.py
@@ -161,6 +161,7 @@ class SpiderWork(object):
         chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(chrome_options = chrome_options)
 
         # driver = webdriver.Chrome()
